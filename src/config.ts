917b6847c32aa03145dcf780d3f4d2362cd7ef1e
TwoC-Mr <1191422391@qq.com>
2026-02-19 20:28:25 +0800

    修改字体

--- src/config.ts
+++ src/config.ts
@@ -55,7 +55,8 @@ export const siteConfig: SiteConfig = {
 		// 顶栏标题文本
 		text: "MrTwoc",
 		// 顶栏标题图标路径，默认使用 public/assets/home/home.png
-		icon: "assets/home/home.png",
+		// icon: "assets/home/home.png",
+		icon: "assets/home/me.png",
 		// 网站Logo图片路径
 		logo: "assets/home/default-logo.png",
 	},
@@ -216,16 +217,16 @@ export const siteConfig: SiteConfig = {
 		asciiFont: {
 			// 英文字体 - 优先级最高
 			// 指定为英文字体则无论字体包含多大范围，都只会保留 ASCII 字符子集
-			fontFamily: "ZenMaruGothic-Medium",
+			fontFamily: "MapleMono-Medium",
 			fontWeight: "400",
-			localFonts: ["ZenMaruGothic-Medium.ttf"],
+			localFonts: ["MapleMono-Medium.ttf"],
 			enableCompress: true, // 启用字体子集优化，减少字体文件大小
 		},
 		cjkFont: {
 			// 中日韩字体 - 作为回退字体
-			fontFamily: "萝莉体 第二版",
+			fontFamily: "MapleMono-Medium",
 			fontWeight: "500",
-			localFonts: ["萝莉体 第二版.ttf"],
+			localFonts: ["MapleMono-Medium.ttf"],
 			enableCompress: true, // 启用字体子集优化，减少字体文件大小
 		},
 	},
