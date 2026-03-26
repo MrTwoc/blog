b5c0260e1b5ad8ed65a6a7e8f0d81b98b627c2bc
TwoC-Mr <1191422391@qq.com>
2026-03-26 21:54:12 +0800

    修改日记功能：支持换行符

--- src/data/diary.ts
+++ src/data/diary.ts
@@ -16,7 +16,7 @@ const diaryData: DiaryItem[] = [
 	{
 		id: 1,
 		content:
-			"这是日记内容:The falling speed of cherry blossoms is five centimeters per second!",
+			"这是日记内容: \nThe falling speed of cherry blossoms is five centimeters per second!",
 		date: "2025-01-15T10:30:00Z",
 		images: ["/images/diary/sakura.jpg", "/images/diary/1.jpg"],
 		// 在线图片示例
